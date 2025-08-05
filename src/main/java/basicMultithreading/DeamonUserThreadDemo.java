135a43df1b1abe84dd907bd876a93eb526eb20cb
trungsliver <buiductrung.cva@gmail.com>
2025-08-05 10:18:52 +0700

    deamon threads vs User threads

---
+++ src/main/java/basicMultithreading/DeamonUserThreadDemo.java
@@ -0,0 +1,42 @@
+package basicMultithreading;
+
+public class DeamonUserThreadDemo {
+    public static void main(String[] args) {
+        Thread backgroundThread = new Thread(new DeamonHelper());
+        Thread userThread = new Thread(new UserHelper());
+
+        // Thiết lập backgroundThread là luồng daemon
+        backgroundThread.setDaemon(true);
+
+        backgroundThread.start();
+        userThread.start();
+    }
+}
+
+class DeamonHelper implements Runnable{
+    @Override
+    public void run() {
+        int count = 0;
+        while (count < 500){
+            try {
+                Thread.sleep(1000); // Giả lập công việc
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            count++;
+            System.out.println("Deamon helper is running ...");
+        }
+    }
+}
+
+class  UserHelper implements Runnable{
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("User helper done with excution !!!");
+    }
+}
